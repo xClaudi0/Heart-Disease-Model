cf3d85990a1e16d5fb29ff0b23b1803d3fcd97e9
Claudio Bellanti <72557566+xClaudi0@users.noreply.github.com>
2021-04-26 18:18:16 +0200

    Update degli spazi per quel coglione di Daniele
    Nulla, Daniele mi rompeva il cazzo per gli spazi.

--- Heart Disease test.py
+++ Heart Disease test.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
-
 # Setuppo i dati
 import pandas as pd
 import numpy as np
 heart_disease = pd.read_csv("/heart-disease.csv")
 heart_disease
-
-
-
-
 # Features matrix
 x = heart_disease.drop("target", axis=1)
 # Labels
 y = heart_disease["target"]
-
-
-
-
-
 # Scelta del modello e hyperparameters
 from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier()
@@ -28,27 +16,12 @@ clf.get_params()
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 clf.fit(x_train, y_train);
-
-
-
-
-
 # Prediction
 y_preds = clf.predict(x_test)
 y_preds
 y_test
-
-
-
-
-
 # Valutazione modello sui train data
 clf.score(x_train, y_train)
-
-
-
-
-
 #Valutazione modello sui test data
 clf.score(x_test, y_test)
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
